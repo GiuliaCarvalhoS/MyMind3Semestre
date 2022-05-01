b5121b193f752fc367b072f91fceb5e2cac3a1c7
Fernando Dias <90704932+FernandoDias18@users.noreply.github.com>
2022-05-01 12:54:08 -0300

    Pagina de alterar cadastro pronta

--- PaginaAlterarDados/js/script.js
+++ PaginaAlterarDados/js/script.js
@@ -1,4 +1,20 @@
-
-function habilitar() {
+function habilitarNome() {
     document.getElementById("nome").disabled = false;
 }
+function habilitarTelefone() {
+    document.getElementById("telefone").disabled = false;
+}
+function habilitarEmail() {
+    document.getElementById("email").disabled = false;
+}
+function habilitarEndereco(){
+    document.getElementById("Endereco").disabled = false;
+}
+function habilitarUsuario(){
+    document.getElementById("usuario").disabled = false;
+}
+function habilitarSenha(){
+    document.getElementById("senha").disabled = false;
+}
+
+
