766fbaff6a0942d908d89064b54d8d66f238ee79
Fernando Dias <90704932+FernandoDias18@users.noreply.github.com>
2022-05-29 15:48:02 -0300

    Criado tabela publicacao

--- Api_MyMind/knexfile.js
+++ Api_MyMind/knexfile.js
@@ -9,10 +9,10 @@ module.exports = {
       client: 'mysql',
       connection: {
       host : '127.0.0.1',
-      port : 3306,
+      port : 3307,
       user : 'root',
       password : 'root',
-      database : 'mymindteste'
+      database : 'mymind'
     },
     migrations: {
       tableName: 'knex_migrations',
