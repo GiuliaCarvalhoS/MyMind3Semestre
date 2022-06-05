9ee3caaf2eb43c8091c41d011e1b0be66d3bd49b
Lucas Eskudlark <62945519+LucasEskudlark@users.noreply.github.com>
2022-06-05 18:58:04 -0300

    Criado o front dos cards no feed

--- PaginaFeed/components/cardPublication/index.js
+++ PaginaFeed/components/cardPublication/index.js
@@ -1,8 +1,35 @@
 
+
+
+
 export const cardPublication = ()=>{
   return `
-  <div class="container">
-    <p class="testando"> aqui fica um card <p>
+  <div class="container-conteudo-card">
+    <div class="container-header-card">
+      <p class="titulo-header">Lucas</p>
+      <p class="usuario-card">@lucas</p>
+    </div>
+
+    <div class="container-img-card"
+      <img src="#" class="img-publi"/>
+
+    </div>
+
+    <div class="container-txt-card">
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing L</p>
+    </div>
+
+    <div class="container-footer-card">
+      <div class="footer-img-card-heart">
+      </div>
+      <div class="footer-img-card-comment">
+      
+      </div>
+      <div class="footer-img-card-share">
+      
+      </div>
+    </div>
+    
   </div>
   
   
